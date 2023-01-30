ec4de24e8bb7d5722e012f15eb0a109d6a025c76
Alex <al.korotkov@intranet-sintek.net>
2023-01-30 15:20:15 +0300

    add underlines in menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,6 +4,8 @@ import closeLogo from '../../images/close.svg';
 
 function Menu(props) {
 
+  const location = useLocation().pathname; 
+
   //Обработчик клика
   function handleClick(evt) {
     if ( (evt.target.classList.contains('menu_visible')) || (evt.target.classList.contains('menu__item')) )
@@ -19,9 +21,9 @@ function Menu(props) {
     <div className={"menu" + (props.isOpen ? " menu_visible" : "")} onClick={handleClick} >
       <ul className={"menu__items" + (props.isOpen ? " menu__items_visible" : "")}>
         <li className="menu__item"><img className="menu__close" src={closeLogo} alt="закрыть" onClick={props.onClose} /></li>
-        <li className="menu__item"><Link to="/" className="menu__main" title="Главная" onClick={props.onClose}>Главная</Link></li>
-        <li className="menu__item"><Link to="/movies" className="menu__link menu__link_active" title="Фильмы" onClick={props.onClose}>Фильмы</Link></li>
-        <li className="menu__item"><Link to="/saved-movies" className="menu__link" title="Сохраненные фильмы" onClick={props.onClose}>Сохраненные фильмы</Link></li>
+        <li className="menu__item"><Link to="/" className={"menu__main" + (location === "/"? " menu__main_active" : "")} title="Главная" onClick={props.onClose}>Главная</Link></li>
+        <li className="menu__item"><Link to="/movies" className={"menu__link" + (location === "/movies"? " menu__link_active" : "")} title="Фильмы" onClick={props.onClose}>Фильмы</Link></li>
+        <li className="menu__item"><Link to="/saved-movies" className={"menu__link" + (location === "/saved-movies"? " menu__link_active" : "")} title="Сохраненные фильмы" onClick={props.onClose}>Сохраненные фильмы</Link></li>
         <li className="menu__item"><Link to="/profile" className="menu__profile" title="Аккаунт" onClick={props.onClose}>Аккаунт</Link></li>
       </ul>
     </div>
